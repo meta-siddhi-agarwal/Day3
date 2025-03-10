c5f1b0532b9c344893aa9e014f4030798bbd0287
meta-siddhi-agarwal <siddhi.agarwal@metacube.com>
2025-03-10 11:19:44 +0530

    MarkSheet.java

--- Assignment3/src/pac1/MarkSheet.java
+++ Assignment3/src/pac1/MarkSheet.java
@@ -13,7 +13,7 @@ public static void main(String[] args) {
 	try 
 	{
 	int n=sc.nextInt();
-	for(int i=0;i<n;i++)
+	for(int i =0 ;i<n;i++)
 	{
 		System.out.println("Enter marks of student "
 		+(i+1));
@@ -22,7 +22,7 @@ public static void main(String[] args) {
 	System.out.println("Select operation to perform\n"
 	+"1.Average of marks\n"+"2.Maximum marks\n"
 	+"3.Minimum marks\n"+"4.Percentage of students passed");
-	int input=sc.nextInt();
+	int input = sc.nextInt();
 	switch(input)
 	{
 	//case 1 to calculate avg. of marks
@@ -48,19 +48,22 @@ public static void main(String[] args) {
 	}
 	catch(Exception e) {
 		System.out.println("Enter valid input");
-	}
-	
+	}	
 	}
 
 //show  average of all marks
 static double average(List<Integer> ls) {
 	int sum=0;
-	for(int i=0;i<ls.size();i++) {
+	for(int i = 0;i<ls.size();i++) {
 		sum+=ls.get(i);
 	}
 	double avg=0;
-	avg=(sum/(ls.size()*1.00));
-	return avg;
+	avg=(sum/(ls.size()*1.0));
+	//here we are multiplying by 100 and then converting into int to allow upto 2 decimal places
+	double ans1=avg*100; 
+	int round=(int)ans1;
+	double result=round/(100*1.00);
+	return result;
 }
 
 //find max. mark
@@ -90,6 +93,9 @@ static double passing_percentage(List<Integer> ls) {
 	double ans=0;
 	double sum=no/(ls.size()*1.0);
 	ans=(sum*100);
+	double ans1=ans*100;
+	int round=(int)ans1;
+	double result=round/(100*1.0);	
 	return ans;
 }
 }
